a3633f647f970d88a5c2e70ec7588919c4e06320
woollysocks <nikitanangia@gmail.com>
2019-12-11 16:01:52 -0500

    more examples in instructions

--- parlai/mturk/tasks/react_task_demo/game1/task_config.py
+++ parlai/mturk/tasks/react_task_demo/game1/task_config.py
@@ -43,35 +43,59 @@ In this task, we have two types of roles: writers and evaluators. Please read th
 <p><strong>Writers:</strong> you will be shown a prompt that describes a situation or event. Using only this description and what you know about the world, please write</p>
 <ul>
 	<li>Write one sentence that is <strong>definitely correct</strong><em>&nbsp;</em>about the situation or event in the prompt.</li>
-	<li>Write one sentence that <strong>might&nbsp;be correct</strong> about the situation or event in the prompt.</li>
 	<li>Write one sentence that is&nbsp;<strong>definitely incorrect</strong>&nbsp;about the situation or event in the prompt.</li>
+	<li>Write one sentence that is&nbsp;<strong>neither</strong> defintiely corrent nor incorrect about the situation or event in the prompt.</li>
 </ul>
 <font color="red">We encourage you to write sentences that are creative and require non-trivial kinds of inference.</font> You bonuses will be based on how evaluators rank you, they will be ranking you based on creativity of your sentences.
 
-<p><strong>Evaluators:</strong> you will be shown a prompt and 3 possible hypothesis for the prompt with the same type of entailment relation (the hypotheses are written by other MTurk workers who are in the writer pool). The three types of entialment relations---definitely correct, might be correct, and definitely incorrect---are explained above in the instructions for writers. <font color="red">Your task as evaluators is to rank the hypothesis sentences in terms of creativity.</font> You bonuses will be based on agreement with other evaluators.</p>
+<p><strong>Evaluators:</strong> you will be shown a prompt and 3 possible hypothesis sentences for the prompt with the same type of entailment label (the hypotheses are written by other MTurk workers who are in the writer pool). The three types of entialment labels---definitely correct, definitely incorrect, and neither---are explained above in the instructions for writers. <font color="red">Your task as evaluators is to rank the hypothesis sentences in terms of creativity.</font> You bonuses will be based on agreement with other evaluators.</p>
 
 
 <u><strong> Examples </u></strong>
-<!-- TODO: COMPLETE THIS. ADD EXAMPLES -->
 <p>
-Prompt: James Jolt moved to London this past summer. <br>
-Label: Defintiely correct
+Prompt: James Jolt moved to London this past summer.
+</p><br>
+
+<p>
+Label: Definitely correct
 Hypothesis: Some months ago James Jolt settled down in England.
-<em> Explanation: There are some synonyms used here, and the reasder needs to know that London is in England.</em>
+<em> Explanation: This example is made more interesting because there are some synonyms used, and the reader needs to know that London is in England.</em>
 </p>
 
-<p>
-Prompt: James Jolt moved to London this past summer. <br>
-Label: Maybe correct
-Hypothesis: James Jolt recently moved across the pond.
-<em> Explanation: If James is moving from the United States, then this hyopthesis would be true. The reader needs to know "the pond" is sometimes used to refer to the Atlantic Ocean.</em>
-</p>
 
 <p>
-Prompt: James Jolt moved to London this past summer. <br>
 Label: Definitely incorrect
 Hypothesis: In spite the Brexit turmoil, James Jolt decided to keep his home and business in London, where they have been for the past decade.
-<em>Explanation: TODO.</em>
+<em>Explanation: From the prompt we know that James just moved to London, and hasn't been there for 10 years. This hypothesis implies that James hasn't just moved to London without discussing moving, making it a more complicated and interesting example.</em>
+</p>
+
+<p>
+Label: Neither (neither definitely correct nor definitely incorrect)
+Hypothesis: James Jolt recently moved across the pond.
+<em> Explanation: If James is moving from the United States, then this hyopthesis would be correct, but this may or may not be true given the prompt. Furthermore, the example is made more interesting because the reader needs to know "the pond" is sometimes used to refer to the Atlantic Ocean.</em>
+</p><br>
+
+<p>
+Prompt: Mother-in-law's tongue is one of the colloquial names for Sansevieria Trifasciata plant. It tolerates most light conditions and doesn't need frequent watering, making it an ideal house plant.
+</p><br>
+
+<p>
+Label: Definitely correct
+Hypothesis: Sansevieria Trifasciatais a good choice for beginners and those who don't want the hassle of a needy plant.
+<em> Explanation: This example is made more interesting by making the non-trivial inference that a plant that tolerates all kinds of light and is easy to water must be easy maintain and therefore good for beginners.</em>
+</p>
+
+
+<p>
+Label: Definitely incorrect
+Hypothesis: Mother in law's tongue doesn't need lots of sunlight but is easy to underwater.
+<em>Explanation: The first part of this hypothesis is true and requires a step of inference from the prompt: tolerating lots of lighting conditions means it can tolerate low sunlight. The second part is a contradiction of the prompt and it uses some rephrasing by saying "underwater."</em>
+</p>
+
+<p>
+Label: Neither (neither definitely correct nor definitely incorrect)
+Hypothesis: Mother in law's tongue is easy to maintain, growing quickly under most conditions.
+<em> Explanation: The premise doesn't state or directly imply that the plant will grow quickly, only that it will live in most conditions. The subtlety of this example makes it interesting.</em>
 </p>
 
 <u><strong> Task Bonuses </u></strong>
